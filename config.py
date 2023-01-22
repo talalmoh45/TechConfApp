46bf58bc6a9c6b88fc133ac5254e1095c01552ab
talalmoh45 <talal.moh45@hotmail.com>
2023-01-22 17:46:22 +0300

    change db

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig:
     DEBUG = True
     POSTGRES_URL="techconfserver.postgres.database.azure.com"  #TODO: Update value
-    POSTGRES_USER="talaladmin" #TODO: Update value
+    POSTGRES_USER="taloo" #TODO: Update value
     POSTGRES_PW="0541431847Aa"   #TODO: Update value
     POSTGRES_DB="techconfdb"   #TODO: Update value
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
